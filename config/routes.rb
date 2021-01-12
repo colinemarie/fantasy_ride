e050b1d967a0415381ced473eccfae7e0ef3d39b
Audrey de Marco <demarco.audrey@gmail.com>
2021-01-12 21:45:05 +0100

    create the show

--- config/routes.rb
+++ config/routes.rb
@@ -4,3 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :vehicles, only: [ :index, :show ]
 end
+
+
+Créer une branche :
+root to: 'vehicles/:id#show'
