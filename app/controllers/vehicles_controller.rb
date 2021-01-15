a0199522187841d042c120493407284332800827
Myriam Faucher Pelletier <myriamfpelletier@gmail.com>
2021-01-15 09:17:43 +0100

    do not merge

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,7 +1,16 @@
 class VehiclesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
   def index
-    @vehicles = Vehicle.all
+    if params[:query].present?
+      sql_query = " \
+      name ILIKE :query \
+      OR description ILIKE :query \
+      OR category ILIKE :query \
+      "
+      @vehicles = Vehicle.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @vehicles = Vehicle.all
+    end
   end
 
   def show
