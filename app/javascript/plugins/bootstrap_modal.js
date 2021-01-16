69742cb9a66942ad7dc2b7039f491ddafae674a8
Coline Juillerat <coline.ju@gmail.com>
2021-01-16 17:04:55 +0100

    modal button

--- app/javascript/plugins/bootstrap_modal.js
+++ app/javascript/plugins/bootstrap_modal.js
@@ -1,8 +1,10 @@
 const initBoostrapModal = () => {
   $(document).ready(function() {
     $('#new_reservation').on('submit', function(e){
+      if ($('#submit_reservation').data('user') === false) {
         $('#logIn').modal('show');
         e.preventDefault();
+      }
     });
   });
 }
