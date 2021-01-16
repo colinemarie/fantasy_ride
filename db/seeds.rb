9dac60d02bb62a4ea2c49073c24a5cd9934c6ec7
Coline Juillerat <coline.ju@gmail.com>
2021-01-16 18:26:42 +0100

    new vehicles

--- db/seeds.rb
+++ db/seeds.rb
@@ -166,6 +166,21 @@ carriage.categories_vehicles.create!(category: wheels)
 
 puts 'carriage created'
 
+carriage = Vehicle.new(name: "Cinderella's carriage", price_per_day: 467,
+                     description: "Looking for a prince?
+                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                      dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                      ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                      fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                      mollit anim id est laborum",
+                     address: 'Near Castle', capacity: 4, minimum_age: 16)
+carriage.user = myriam
+file = URI.open('https://res.cloudinary.com/dw3inosxv/image/upload/v1610817452/fantasy-ride/cendrillon_c6ubo0.jpg')
+carriage.photos.attach(io: file, filename: 'carriage.png', content_type: 'image/png')
+carriage.save!
+carriage.categories_vehicles.create!(category: wheels)
+
+puts 'carriage created'
 
 puts 'vehicles done'
 
