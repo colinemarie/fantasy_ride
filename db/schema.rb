60fb9dfe508f8b6b90d7235c16030cb22cf0feab
Audrey de Marco <demarco.audrey@gmail.com>
2021-01-16 13:00:38 +0100

    update layout show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_083207) do
+ActiveRecord::Schema.define(version: 2021_01_13_162109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_01_14_083207) do
 
   create_table "vehicles", force: :cascade do |t|
     t.string "name"
+    t.string "category"
     t.integer "price_per_day"
     t.text "description"
     t.text "address"
