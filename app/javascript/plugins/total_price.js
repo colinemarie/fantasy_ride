e1b1a0196bac59366f3b670145d97a53c6b0828b
Coline Juillerat <coline.ju@gmail.com>
2021-01-21 21:04:42 +0100

    total price updated

--- app/javascript/plugins/total_price.js
+++ app/javascript/plugins/total_price.js
@@ -1,14 +1,14 @@
-const calculateTotalPrice = () => {
-  const dailyPrice = document.getElementById("daily-price")
-  const startDate = (flatpickr("#reservation_start_date", {})).selectedDates[0];
-  const endDate = (flatpickr("#reservation_end_date", {})).selectedDates[0];
+// const calculateTotalPrice = () => {
+//   const dailyPrice = document.getElementById("daily-price")
+//   const startDate = (flatpickr("#reservation_start_date", {})).selectedDates[0];
+//   const endDate = (flatpickr("#reservation_end_date", {})).selectedDates[0];
   // const startDate = document.getElementById("reservation_start_date")
   // const endDate = document.getElementById("reservation_end_date")
-  const totalPrice = document.getElementById("total-price")
+//   const totalPrice = document.getElementById("total-price")
 
-  const totalDays = parseInt(endDate) - parseInt(startDate)
-  totalPrice.innerText = totalDays * parseInt(dailyPrice.innerText)
-};
+//   const totalDays = parseInt(endDate) - parseInt(startDate)
+//   totalPrice.innerText = totalDays * parseInt(dailyPrice.innerText)
+// };
 
 // const changeAutomaticallyEndDate = () => {
   // const startDate = (flatpickr("#reservation_start_date", {})).selectedDates[0];
@@ -21,21 +21,21 @@ const calculateTotalPrice = () => {
 //   calculateTotalPrice();
 // };
 
-const initTotalPrice = () => {
-  const dailyPrice = document.getElementById("daily-price")
-  if (!dailyPrice) return;
+// const initTotalPrice = () => {
+//   const dailyPrice = document.getElementById("daily-price")
+//   if (!dailyPrice) return;
 
-  const startDate = document.getElementById("reservation_start_date")
-  const endDate = document.getElementById("reservation_end_date")
+//   const startDate = document.getElementById("reservation_start_date")
+//   const endDate = document.getElementById("reservation_end_date")
   // const startDate = (flatpickr("#reservation_start_date", {})).selectedDates[0];
   // const endDate = (flatpickr("#reservation_end_date", {})).selectedDates[0];
 
-  startDate.addEventListener("change", calculateTotalPrice)
-  // startDate.addEventListener("change", changeAutomaticallyEndDate)
-  // endDate.addEventListener("change", changeAutomaticallyEndDate)
-  endDate.addEventListener("change", calculateTotalPrice)
-};
+//   startDate.addEventListener("change", calculateTotalPrice)
+//   // startDate.addEventListener("change", changeAutomaticallyEndDate)
+//   // endDate.addEventListener("change", changeAutomaticallyEndDate)
+//   endDate.addEventListener("change", calculateTotalPrice)
+// };
 
-export {
-  initTotalPrice
-}
+// export {
+//   initTotalPrice
+// }
