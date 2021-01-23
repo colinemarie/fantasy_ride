1d31bbae263d709c0d443c0c90b72bc5582feede
Coline Juillerat <coline.ju@gmail.com>
2021-01-23 17:19:05 +0100

    disable

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -4,10 +4,13 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 const initFlatpickr = () => {
   const vehicleBooking = document.getElementById("vehicle-booking-dates")
   if (!vehicleBooking) return;
+    const unavailableDates = JSON.parse(document.querySelector('#vehicle-booking-dates').dataset.unavailable)
+    console.log(unavailableDates);
     flatpickr("#reservation_start_date", {
       plugins: [new rangePlugin({ input: "#reservation_end_date"})],
       minDate: new Date().fp_incr(1),
-      dateFormat: "d-m-Y",
+      dateFormat: "Y-m-d",
+      disable: unavailableDates,
       locale: {
       "firstDayOfWeek": 1
         },
@@ -22,7 +25,6 @@ const initFlatpickr = () => {
 
 
 export { initFlatpickr }
-
 
 
 
